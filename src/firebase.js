203c22afa3d66a369063e002cf821af19efa1d85
KleytonFSantos <akatsukipb12@gmail.com>
2022-02-28 17:00:57 -0300

    master

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyB0KcZ678ejFClc_GuPmgt3YdCOzPeclfQ",
+    authDomain: "clallenge-dffb7.firebaseapp.com",
+    projectId: "clallenge-dffb7",
+    storageBucket: "clallenge-dffb7.appspot.com",
+    messagingSenderId: "558123605888",
+    appId: "1:558123605888:web:8aba14e476720327efeb65",
+    measurementId: "G-FFVHJYTQNM"
+  };
